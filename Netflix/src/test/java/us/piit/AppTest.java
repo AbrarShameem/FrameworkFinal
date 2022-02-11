4f69aab97e2a2485acbb0804c8f88bff946854c7
Dani30405NY <zedh811d@gmail.com>
2022-02-10 17:52:04 -0800

    danielle updates

--- Netflix/src/test/java/us/piit/AppTest.java
+++ Netflix/src/test/java/us/piit/AppTest.java
@@ -1,6 +1,33 @@
 package us.piit;
 
-public class AppTest 
-{
+import base.CommonAPI;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+public class AppTest extends CommonAPI{
+
+    HomePage home = new HomePage();
+
+    @Test
+    public void signIntoNetflix() {
+        HomePage home = PageFactory.initElements(driver, HomePage.class);
+        home.clickOnSignInButton();
+        waitFor(3);
+    }
+
+    @Test
+    public void
+
+
+
+
+
+
+
+
+    }
+
+
+
 
 }
