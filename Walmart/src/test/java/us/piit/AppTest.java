b03c5f6ed109b5e4fea1de01a42605bc13bde5a8
Imtiaz Ahmed <imtiazahmed@Imtiazs-MacBook-Pro.local>
2022-02-12 15:24:18 -0500

    imtiaz

--- Walmart/src/test/java/us/piit/AppTest.java
+++ Walmart/src/test/java/us/piit/AppTest.java
@@ -1,8 +1,27 @@
 package us.piit;
 
-public class AppTest 
-{
+import base.CommonAPI;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+public class AppTest extends CommonAPI {
+
+    HomePage home = new HomePage();
+
+    //@Test
+    public void test1() {
+        HomePage home = PageFactory.initElements(driver, HomePage.class);
+        home.firstTest();
+    }
+    //@Test
+    public void test2() {
+        HomePage home = PageFactory.initElements(driver, HomePage.class);
+        home.secondTest();
+    }
+    @Test
+    public void test3() {
+        HomePage home = PageFactory.initElements(driver, HomePage.class);
+        home.testTest();
 
 
-
-}
+    }}
