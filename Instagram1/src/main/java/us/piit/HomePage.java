df97c2f87855349f8d42543ee201312fbd62a9ab
Mou6097 <mou6097@gmail.com>
2022-02-16 22:31:50 -0500

    updates

--- Instagram1/src/main/java/us/piit/HomePage.java
+++ Instagram1/src/main/java/us/piit/HomePage.java
@@ -2,9 +2,11 @@ package us.piit;
 
 import base.CommonAPI;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -12,35 +14,99 @@ import java.util.List;
 public class HomePage extends CommonAPI {
 
 
-    @FindBy (xpath = "//div[contains(text(),'Log In')]")
+    @FindBy(xpath = "//div[contains(text(),'Log In')]")
     WebElement login;
 
-    @FindBy (xpath="//body/div[@id='react-root']/section[1]/main[1]/article[1]/div[2]/div[1]/div[1]/form[1]/div[1]/div[1]/div[1]/label[1]/input[1]")
+    @FindBy (xpath ="//button[contains(text(),'Not now')]")
+    WebElement notnow;
+
+    @FindBy(xpath = "//body/div[@id='react-root']/section[1]/main[1]/article[1]/div[2]/div[1]/div[1]/form[1]/div[1]/div[1]/div[1]/label[1]/input[1]")
     WebElement email;
 
-    @FindBy (xpath="//body/div[@id='react-root']/section[1]/main[1]/article[1]/div[2]/div[1]/div[1]/form[1]/div[1]/div[2]/div[1]/label[1]/input[1]")
+    @FindBy(xpath = "//body/div[@id='react-root']/section[1]/main[1]/article[1]/div[2]/div[1]/div[1]/form[1]/div[1]/div[2]/div[1]/label[1]/input[1]")
     WebElement password;
+
+    @FindBy(xpath = "//body/div[@id='react-root']/section[1]/nav[1]/div[2]/div[1]/div[1]/div[3]/div[1]/div[6]/span[1]/img[1]")
+    WebElement Profile;
+
+    @FindBy(xpath = "//div[contains(text(),'Profile')]")
+    WebElement onProfile;
+
+    @FindBy(xpath = "//*[text()='Saved']")
+    WebElement saved;
+
+    @FindBy(xpath = "//div[contains(text(),'Settings')]")
+    WebElement settings;
+
+    @FindBy(xpath = "//body/div[@id='react-root']/section[1]/nav[1]/div[2]/div[1]/div[1]/div[3]/div[1]/div[4]/a[1]/*[1]")
+    WebElement polygon;
+
+    @FindBy(xpath = "//body/div[@id='react-root']/section[1]/nav[1]/div[2]/div[1]/div[1]/div[3]/div[1]/div[2]/a[1]/*[1]")
+    WebElement sendmessage;
+
+    @FindBy(xpath = "//header/section[1]/ul[1]/li[3]/a[1]/div[1]")
+    WebElement following;
+
+    @FindBy(xpath = "//span[contains(text(),'Search')]")
+    WebElement Search;
+
+    public void onFollowing() {
+        click(following);
+    }
+
+    public void OnSaved() {
+        click(saved);
+    }
 
     //public void Test01(){
 
-        //username.sendKeys("sellingsunset007");
-        //password.sendKeys("sunset1234");
-        //login.click();
-        //waitFor(3);
-        public void login() {
-            type(email,"tubaf64096@gmail.com");
-            type(password,"Sunset123");
-            click(login);
+    //username.sendKeys("sellingsunset007");
+    //password.sendKeys("Sunset1234");
+    //login.click();
+    //waitFor(3);
+    public void login() {
+        type(email, "tubaf64096@gmail.com");
+        type(password, "Sunset123");
+        click(login);
     }
+
     public void Wrongusername() {
-        type(email,"new@gmail.com");
-        type(password,"Sunset123");
+        type(email, "new@gmail.com");
+        type(password, "Sunset123");
         click(login);
     }
+
     public void Wrongpassword() {
-        type(email,"new@gmail.com");
-        type(password,"Sunset123");
+        type(email, "new@gmail.com");
+        type(password, "Sunset123");
         click(login);
+    }
+
+    public void onSearch() {
+        type(Search, "angelinajolie");
+        waitFor(3);
+        Search.sendKeys(Keys.ARROW_DOWN);
+        Search.sendKeys(Keys.ENTER);
+    }
+
+
+    public void profilemain(){
+        click(Profile);
+    }
+    public void secondProfile(){
+        click(onProfile);
+    }
+    public void onSettings(){
+        click(settings);
+    }
+    public void onPolygon(){
+        click(polygon);
+    }
+    public void onSendMessage(){
+        click(sendmessage);
+    }
+    public void loginNow(){
+        click(notnow);
     }
     }
 
--- Instagram1/src/test/java/us/piit/AppTest.java
+++ Instagram1/src/test/java/us/piit/AppTest.java
@@ -8,14 +8,13 @@ import us.piit.HomePage;
 public class AppTest extends CommonAPI{
     HomePage home = new HomePage();
 
-    //@Test
+   //@Test
     public void test01(){
         HomePage home = PageFactory.initElements(driver, HomePage.class);
         //home.first(Test);
         home.login();
         waitFor(10);
     }
-
 
 
    //@Test
@@ -27,13 +26,125 @@ public class AppTest extends CommonAPI{
        waitFor(5);
 
     }
-    @Test
+    //@Test
     public void test03(){
         HomePage home = PageFactory.initElements(driver, HomePage.class);
-        home.Wrongpassword();
+       // home.Wrongpassword();
         //home.first(Test);
         // home.enterWrongEmail();
         waitFor(5);
     }
 
+
+
+    //@Test
+    public void test04() {
+         HomePage home = PageFactory.initElements(driver, HomePage.class);
+       ////home.Userscrolldown();
+        //home.first(Test);
+        // home.enterEmail();
+        waitFor(5);
     }
+
+    //@Test
+    public void test05(){
+        HomePage home = PageFactory.initElements(driver, HomePage.class);
+        //home.first(Test);
+        home.login();
+        waitFor(10);
+       home.onSearch();
+        waitFor(6);
+    }
+    @Test
+    public void test06(){
+        HomePage home = PageFactory.initElements(driver, HomePage.class);
+        //home.first(Test);
+        home.login();
+        waitFor(2);
+        home.loginNow();
+       waitFor(6);
+    }
+    //@Test
+    public void test07(){
+        HomePage home = PageFactory.initElements(driver, HomePage.class);
+        //home.first(Test);
+        home.login();
+        waitFor(5);
+        //home.loginNow();
+        waitFor(6);
+    }
+    //@Test
+    public void test08(){
+        HomePage home = PageFactory.initElements(driver, HomePage.class);
+        //home.first(Test);
+        home.login();
+        waitFor(5);
+       // home.loginNow();
+        waitFor(6);
+        home.onSearch();
+        waitFor(7);
+    }
+   // @Test
+    public void test09(){
+        HomePage home = PageFactory.initElements(driver, HomePage.class);
+        home.login();
+        waitFor(5);
+       home.profilemain();
+       waitFor(4);}
+    //@Test
+    public void test10(){
+        HomePage home = PageFactory.initElements(driver, HomePage.class);
+        home.login();
+        waitFor(5);
+        home.profilemain();
+        waitFor(6);
+        home.secondProfile();
+        waitFor(4);}
+   // @Test
+    public void test11(){
+        HomePage home = PageFactory.initElements(driver, HomePage.class);
+        home.login();
+        waitFor(5);
+        home.profilemain();
+        waitFor(6);
+        home.OnSaved();
+        waitFor(4);}
+     //@Test
+    public void test12(){
+        HomePage home = PageFactory.initElements(driver, HomePage.class);
+        home.login();
+        waitFor(5);
+        home.profilemain();
+        waitFor(6);
+        home.onSettings();
+        waitFor(4);}
+
+   // @Test
+    public void test13(){
+        HomePage home = PageFactory.initElements(driver, HomePage.class);
+        home.login();
+        waitFor(5);
+        home.onPolygon();
+        waitFor(5);}
+    //@Test
+    public void test14(){
+        HomePage home = PageFactory.initElements(driver, HomePage.class);
+        home.login();
+        waitFor(5);
+        home.onSendMessage();
+        waitFor(5);}
+   // @Test
+    public void test15(){
+        HomePage home = PageFactory.initElements(driver, HomePage.class);
+        home.login();
+        waitFor(5);
+        home.profilemain();
+        waitFor(6);
+        home.secondProfile();
+        waitFor(4);
+    home.onFollowing();
+    waitFor(5);}
+
+
+}
+
