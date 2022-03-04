4bfd74576269b0e9fe3b7fa303195d7f3b41faee
Imtiaz Ahmed <imtiazahmed@Imtiazs-MacBook-Pro.local>
2022-03-03 20:47:22 -0500

    method

--- Walmart/src/main/java/us/piit/HomeFurnitureItem.java
+++ Walmart/src/main/java/us/piit/HomeFurnitureItem.java
@@ -1,4 +1,22 @@
 package us.piit;
 
-public class HomeFurnitureItem {
+import base.CommonAPI;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+ import org.openqa.selenium.interactions.Action;
+ import org.testng.Assert;
+
+public class  HomeFurnitureItem extends CommonAPI {
+    public HomeFurnitureItem(WebDriver driver) {
+        super.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//span[contains(text(),'All Categories')]")
+    WebElement AllCategories;
+
+
+
 }
