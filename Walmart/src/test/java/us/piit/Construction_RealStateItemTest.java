75e9b512005ce2ea40a503c168ec9802373311be
Imtiaz Ahmed <imtiazahmed@Imtiazs-MBP.fios-router.home>
2022-03-05 01:31:09 -0500

    done

--- Walmart/src/test/java/us/piit/Construction_RealStateItemTest.java
+++ Walmart/src/test/java/us/piit/Construction_RealStateItemTest.java
@@ -1,4 +1,112 @@
 package us.piit;
 
-public class Construction_RealStateItemTest {
-}
+import base.CommonAPI;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+    public class Construction_RealStateItemTest extends CommonAPI {
+
+         @Test
+        public void construction_RealStateItemTest1() {
+            HomePage homePage = new HomePage(driver);
+            Construction_RealStateItem construction_RealStateItem = new Construction_RealStateItem(driver);
+            homePage.clickAllCategories();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.alibaba.com");
+            construction_RealStateItem.clickConstruction_RealState();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.alibaba.com/Construction-Real-Estate_p13?spm=a2700.8293689.HomeLeftCategory.d13.4b7667afXzSpMs");
+            construction_RealStateItem.clickBathroom_Kitchen();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.alibaba.com/trade/search?spm=a27aq.13929341.IndustryCategory.2.454a1997EfcxlL&CateId=100006265&SearchText=Bathroom+%26+Kitchen&language=en&indexArea=product_en");
+
+        }
+
+         @Test
+        public void construction_RealStateItemTest2() {
+            HomePage homePage = new HomePage(driver);
+            Construction_RealStateItem construction_RealStateItem = new Construction_RealStateItem(driver);
+            homePage.clickAllCategories();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.alibaba.com");
+            construction_RealStateItem.clickConstruction_RealState();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.alibaba.com/Construction-Real-Estate_p13?spm=a2700.8293689.HomeLeftCategory.d13.4b7667afXzSpMs");
+            construction_RealStateItem.clickBathroom_Kitchen();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.alibaba.com/trade/search?spm=a27aq.13929341.IndustryCategory.2.454a1997EfcxlL&CateId=100006265&SearchText=Bathroom+%26+Kitchen&language=en&indexArea=product_en");
+            construction_RealStateItem.clickBathroomVanities();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.alibaba.com/trade/search?spm=a27aq.13929341.IndustryCategory.34.79c71997tXFo7d&CateId=130180&SearchText=Bathroom+Vanities&language=en&indexArea=product_en");
+            construction_RealStateItem.clickKitchenSink();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.alibaba.com/trade/search?spm=a27aq.13929341.IndustryCategory.46.79c71997tXFo7d&CateId=130110&SearchText=Bathroom+Sinks&language=en&indexArea=product_en");
+        }
+
+         @Test
+        public void construction_RealStateItemTest3() {
+            HomePage homePage = new HomePage(driver);
+            Construction_RealStateItem construction_RealStateItem = new Construction_RealStateItem(driver);
+            homePage.clickAllCategories();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.alibaba.com");
+            construction_RealStateItem.clickConstruction_RealState();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.alibaba.com/Construction-Real-Estate_p13?spm=a2700.8293689.HomeLeftCategory.d13.4b7667afXzSpMs");
+            construction_RealStateItem.clickBuilding_IndustrialGlass();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.alibaba.com/trade/search?spm=a27aq.13929341.IndustryCategory.3.79c71997tXFo7d&CateId=201723103&SearchText=Building+%26+Industrial+Glass&language=en&indexArea=product_en");
+            construction_RealStateItem.clickBulletProofGlass();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.alibaba.com/trade/search?spm=a27aq.13929341.IndustryCategory.46.79c71997tXFo7d&CateId=130110&SearchText=Bathroom+Sinks&language=en&indexArea=product_en");
+            construction_RealStateItem.clickElectricGlass();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.alibaba.com/trade/search?spm=a27aq.13929341.IndustryCategory.61.79c71997tXFo7d&CateId=201723603&SearchText=Electronic+glass&language=en&indexArea=product_en");
+        }
+
+         @Test
+        public void construction_RealStateItemTest4() {
+            HomePage homePage = new HomePage(driver);
+            Construction_RealStateItem construction_RealStateItem = new Construction_RealStateItem(driver);
+            homePage.clickAllCategories();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.alibaba.com");
+            construction_RealStateItem.clickConstruction_RealState();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.alibaba.com/Construction-Real-Estate_p13?spm=a2700.8293689.HomeLeftCategory.d13.4b7667afXzSpMs");
+            construction_RealStateItem.clickBuilding_IndustrialGlass();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.alibaba.com/trade/search?spm=a27aq.13929341.IndustryCategory.3.79c71997tXFo7d&CateId=201723103&SearchText=Building+%26+Industrial+Glass&language=en&indexArea=product_en");
+            construction_RealStateItem.clickBulletProofGlass();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.alibaba.com/trade/search?spm=a27aq.13929341.IndustryCategory.46.79c71997tXFo7d&CateId=130110&SearchText=Bathroom+Sinks&language=en&indexArea=product_en");
+            construction_RealStateItem.clickElectricGlass();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.alibaba.com/trade/search?spm=a27aq.13929341.IndustryCategory.61.79c71997tXFo7d&CateId=201723603&SearchText=Electronic+glass&language=en&indexArea=product_en");
+            construction_RealStateItem.clickTintedGlass();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.alibaba.com/trade/search?spm=a27aq.13929341.IndustryCategory.85.79c71997tXFo7d&CateId=201733701&SearchText=Tinted+Glass&language=en&indexArea=product_en");
+        }
+         @Test
+        public void construction_RealStateItemTest5() {
+            HomePage homePage = new HomePage(driver);
+            Construction_RealStateItem construction_RealStateItem = new Construction_RealStateItem(driver);
+            homePage.clickAllCategories();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.alibaba.com");
+            construction_RealStateItem.clickConstruction_RealState();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.alibaba.com/Construction-Real-Estate_p13?spm=a2700.8293689.HomeLeftCategory.d13.4b7667afXzSpMs");
+            construction_RealStateItem.clickCeilings();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.alibaba.com/trade/search?spm=a27aq.13929341.IndustryCategory.5.79c71997tXFo7d&CateId=100007099&SearchText=Ceilings&language=en&indexArea=product_enq");
+
+        }
+
+         @Test
+        public void construction_RealStateItemTest6() {
+            HomePage homePage = new HomePage(driver);
+            Construction_RealStateItem construction_RealStateItem = new Construction_RealStateItem(driver);
+            homePage.clickAllCategories();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.alibaba.com");
+            construction_RealStateItem.clickConstruction_RealState();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.alibaba.com/Construction-Real-Estate_p13?spm=a2700.8293689.HomeLeftCategory.d13.4b7667afXzSpMs");
+            construction_RealStateItem.clickCeilings();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.alibaba.com/trade/search?spm=a27aq.13929341.IndustryCategory.5.79c71997tXFo7d&CateId=100007099&SearchText=Ceilings&language=en&indexArea=product_enq");
+            construction_RealStateItem.clickCornerGuards();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.alibaba.com/trade/search?spm=a27aq.13929341.IndustryCategory.6.79c71997tXFo7d&CateId=1340&SearchText=Corner+Guards&language=en&indexArea=product_en");
+
+        }
+         @Test
+        public void construction_RealStateItemTest7() {
+            HomePage homePage = new HomePage(driver);
+            Construction_RealStateItem construction_RealStateItem = new Construction_RealStateItem(driver);
+            homePage.clickAllCategories();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.alibaba.com");
+            construction_RealStateItem.clickConstruction_RealState();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.alibaba.com/Construction-Real-Estate_p13?spm=a2700.8293689.HomeLeftCategory.d13.4b7667afXzSpMs");
+            construction_RealStateItem.clickCeilings();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.alibaba.com/trade/search?spm=a27aq.13929341.IndustryCategory.5.79c71997tXFo7d&CateId=100007099&SearchText=Ceilings&language=en&indexArea=product_enq");
+            construction_RealStateItem.clickTops();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.alibaba.com/trade/search?spm=a27aq.13929341.IndustryCategory.7.79c71997tXFo7d&CateId=131210&SearchText=Countertops%2CVanity+Tops+%26+Table+Tops&language=en&indexArea=product_en");
+        }
+    }
--- Walmart/src/test/java/us/piit/FurnitureItemTest.java
+++ Walmart/src/test/java/us/piit/FurnitureItemTest.java
@@ -1,4 +1,213 @@
 package us.piit;
 
-public class FurnitureItemTest {
-}
+import base.CommonAPI;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+    public class FurnitureItemTest extends CommonAPI {
+
+
+        @Test
+        public void FurnitureItemTest1() {
+            HomePage homePage = new HomePage(driver);
+            FurnitureItem furnitureItem = new FurnitureItem(driver);
+            homePage.clickAllCategories();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/");
+            furnitureItem.clickFurniture();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/Furniture_p1503?spm=a2700.8293689.HomeLeftCategory.d1503.4b7667afnN85YC");
+            goBack();
+            driver.navigate().to("https://www.alibaba.com/");
+
+        }
+
+        @Test
+        public void FurnitureItemTest2() {
+            HomePage homePage = new HomePage(driver);
+            FurnitureItem furnitureItem = new FurnitureItem(driver);
+            homePage.clickAllCategories();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/");
+            furnitureItem.clickFurniture();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/Furniture_p1503?spm=a2700.8293689.HomeLeftCategory.d1503.4b7667afnN85YC");
+            goBack();
+            driver.navigate().to("https://www.alibaba.com/");
+            homePage.clickAllCategories();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/");
+            furnitureItem.clickFurniture();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/Furniture_p1503?spm=a2700.8293689.HomeLeftCategory.d1503.4b7667afnN85YC");
+            furnitureItem.clickCommercialFurniture();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/trade/search?spm=a27aq.13929384.IndustryCategory.1.55271d2aVsW92O&CateId=150301&SearchText=Commercial+Furniture&language=en&indexArea=product_en");
+        }
+
+        @Test
+        public void FurnitureItemTest3() {
+            HomePage homePage = new HomePage(driver);
+            FurnitureItem furnitureItem = new FurnitureItem(driver);
+            homePage.clickAllCategories();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/");
+            furnitureItem.clickFurniture();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/Furniture_p1503?spm=a2700.8293689.HomeLeftCategory.d1503.4b7667afnN85YC");
+            goBack();
+            driver.navigate().to("https://www.alibaba.com/");
+            homePage.clickAllCategories();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/");
+            furnitureItem.clickFurniture();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/Furniture_p1503?spm=a2700.8293689.HomeLeftCategory.d1503.4b7667afnN85YC");
+            furnitureItem.clickCommercialFurniture();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/trade/search?spm=a27aq.13929384.IndustryCategory.1.55271d2aVsW92O&CateId=150301&SearchText=Commercial+Furniture&language=en&indexArea=product_en");
+            furnitureItem.clickFurnitureAccessories();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/trade/search?spm=a27aq.13929384.IndustryCategory.2.55271d2aVsW92O&CateId=201346305&SearchText=Furniture+Accessories&language=en&indexArea=product_en");
+            goBack();
+            furnitureItem.clickAllCategories();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/");
+        }
+
+        @Test
+        public void FurnitureItemTest4() {
+            HomePage homePage = new HomePage(driver);
+            FurnitureItem furnitureItem = new FurnitureItem(driver);
+            homePage.clickAllCategories();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/");
+            furnitureItem.clickFurniture();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/Furniture_p1503?spm=a2700.8293689.HomeLeftCategory.d1503.4b7667afnN85YC");
+            goBack();
+            driver.navigate().to("https://www.alibaba.com/");
+            homePage.clickAllCategories();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/");
+            furnitureItem.clickFurnitureHardware();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/trade/search?spm=a27aq.13929384.IndustryCategory.3.55271d2aVsW92O&CateId=150306&SearchText=Furniture+Hardware&language=en&indexArea=product_en");
+
+
+        }
+
+        @Test
+        public void FurnitureItemTest5() {
+            HomePage homePage = new HomePage(driver);
+            FurnitureItem furnitureItem = new FurnitureItem(driver);
+            homePage.clickAllCategories();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/");
+            furnitureItem.clickFurniture();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/Furniture_p1503?spm=a2700.8293689.HomeLeftCategory.d1503.4b7667afnN85YC");
+            goBack();
+            driver.navigate().to("https://www.alibaba.com/");
+            homePage.clickAllCategories();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/");
+            furnitureItem.clickFurnitureHardware();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/trade/search?spm=a27aq.13929384.IndustryCategory.3.55271d2aVsW92O&CateId=150306&SearchText=Furniture+Hardware&language=en&indexArea=product_en");
+            goBack();
+            homePage.clickAllCategories();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/");
+            furnitureItem.clickFurnitureParts();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/trade/search?spm=a27aq.13929384.IndustryCategory.4.55271d2aVsW92O&CateId=3708&SearchText=Furniture+Parts&language=en&indexArea=product_en");
+
+
+        }
+
+        @Test
+        public void FurnitureItemTest6() {
+            HomePage homePage = new HomePage(driver);
+            FurnitureItem furnitureItem = new FurnitureItem(driver);
+            homePage.clickAllCategories();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/");
+            furnitureItem.clickFurniture();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/Furniture_p1503?spm=a2700.8293689.HomeLeftCategory.d1503.4b7667afnN85YC");
+            goBack();
+            driver.navigate().to("https://www.alibaba.com/");
+            homePage.clickAllCategories();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/");
+            furnitureItem.clickFurnitureHardware();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/trade/search?spm=a27aq.13929384.IndustryCategory.3.55271d2aVsW92O&CateId=150306&SearchText=Furniture+Hardware&language=en&indexArea=product_en");
+            goBack();
+            homePage.clickAllCategories();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/");
+            furnitureItem.clickHomeFurniture();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/trade/search?spm=a27aq.13929384.IndustryCategory.5.55271d2aVsW92O&CateId=150303&SearchText=Home+Furniture&language=en&indexArea=product_en");
+
+        }
+
+        @Test
+        public void FurnitureItemTest7() {
+            HomePage homePage = new HomePage(driver);
+            FurnitureItem furnitureItem = new FurnitureItem(driver);
+            homePage.clickAllCategories();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/");
+            furnitureItem.clickFurniture();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/Furniture_p1503?spm=a2700.8293689.HomeLeftCategory.d1503.4b7667afnN85YC");
+            goBack();
+            driver.navigate().to("https://www.alibaba.com/");
+            homePage.clickAllCategories();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/");
+            furnitureItem.clickFurnitureHardware();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/trade/search?spm=a27aq.13929384.IndustryCategory.3.55271d2aVsW92O&CateId=150306&SearchText=Furniture+Hardware&language=en&indexArea=product_en");
+            goBack();
+            homePage.clickAllCategories();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/");
+            furnitureItem.clickKidsFurniture();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/trade/search?spm=a27aq.13929384.IndustryCategory.6.55271d2aVsW92O&CateId=100003020&SearchText=Kid%27s+Furniture&language=en&indexArea=product_en");
+
+        }
+
+        @Test
+        public void FurnitureItemTest8() {
+            HomePage homePage = new HomePage(driver);
+            FurnitureItem furnitureItem = new FurnitureItem(driver);
+            homePage.clickAllCategories();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/");
+            furnitureItem.clickFurniture();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/Furniture_p1503?spm=a2700.8293689.HomeLeftCategory.d1503.4b7667afnN85YC");
+            goBack();
+            driver.navigate().to("https://www.alibaba.com/");
+            homePage.clickAllCategories();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/");
+            furnitureItem.clickFurnitureHardware();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/trade/search?spm=a27aq.13929384.IndustryCategory.3.55271d2aVsW92O&CateId=150306&SearchText=Furniture+Hardware&language=en&indexArea=product_en");
+            goBack();
+            homePage.clickAllCategories();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/");
+            furnitureItem.clickOtherFurniture();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/trade/search?spm=a27aq.13929384.IndustryCategory.7.55271d2aVsW92O&CateId=150399&SearchText=Other+Furniture&language=en&indexArea=product_en");
+
+        }
+
+        @Test
+        public void FurnitureItemTest9() {
+            HomePage homePage = new HomePage(driver);
+            FurnitureItem furnitureItem = new FurnitureItem(driver);
+            homePage.clickAllCategories();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/");
+            furnitureItem.clickFurniture();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/Furniture_p1503?spm=a2700.8293689.HomeLeftCategory.d1503.4b7667afnN85YC");
+            goBack();
+            driver.navigate().to("https://www.alibaba.com/");
+            homePage.clickAllCategories();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/");
+            furnitureItem.clickFurnitureHardware();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/trade/search?spm=a27aq.13929384.IndustryCategory.3.55271d2aVsW92O&CateId=150306&SearchText=Furniture+Hardware&language=en&indexArea=product_en");
+            goBack();
+            homePage.clickAllCategories();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/");
+            furnitureItem.clickOutdoorFurniture();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/trade/search?spm=a27aq.13929384.IndustryCategory.8.55271d2aVsW92O&CateId=150302&SearchText=Outdoor+Furniture&language=en&indexArea=product_en");
+
+        }
+
+        @Test
+        public void FurnitureItemTest10() {
+            HomePage homePage = new HomePage(driver);
+            FurnitureItem furnitureItem = new FurnitureItem(driver);
+            homePage.clickAllCategories();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/");
+            furnitureItem.clickFurniture();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/Furniture_p1503?spm=a2700.8293689.HomeLeftCategory.d1503.4b7667afnN85YC");
+            goBack();
+            driver.navigate().to("https://www.alibaba.com/");
+            homePage.clickAllCategories();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/");
+            furnitureItem.clickHospitalFurniture();
+            Assert.assertEquals(getTitle(), "https://www.alibaba.com/trade/search?spm=a27aq.13929384.IndustryCategory.25.458d1d2aQPaP0e&CateId=100001030&SearchText=Hospital+Furniture&language=en&indexArea=product_en");
+
+        }
+
+
+    }
